ce141929d8abb297cff5836618a4e064a02f2253
yangchen <gyachungriom@gmail.com>
2021-09-07 17:35:56 +0200

    students prêt

--- chapter_2/students.js
+++ chapter_2/students.js
@@ -1,51 +1,63 @@
-var students = [
-    {
-        name: "Peter",
-        surname: "Andrews",
-        nationality: "UK",
-    },
-    {
-        name: "Sandra",
-        surname: "bullocks",
-        nationality: "UK",
-    },
-    {
-        name: "Anne",
-        surname: "Froncoise",
-        nationality: "US",
-    },
-    {
-        name: "Julie",
-        surname: "Martin",
-        nationality: "France",
-    },
-]
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
+//to access config.env 
+const dotenv = require("dotenv");
+dotenv.config({
+	path: "../config.env",
+});
+const mongoose = require("mongoose");
+
+
+//connetion to mongoose
+mongoose
+	.connect(process.env.DB, {
+		useNewUrlParser: true,
+	})
+	.then(() => {
+		console.log("Connected to MongoDB !");
+	});
+
+  //Mongoose Schema 
+  const StudentSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    surname:{
+      type: String,
+    },
+    nationality : String,
+  });
+  const Student = mongoose.model("Student", StudentSchema);
+
+app.get('/', async (_req, res) => {
+    const student= await Student.find();
     res.json({status :'ok',
-            data : students});
+            data : student
+        });
 });
 
-app.get('/students', (req, res) => {
-    const listNames= students.map(a=>a.name) ;
-    res.json(listNames)
+app.get('/students/:id', async (req, res) => {
+    const student= await Student.findById(req.params.id);
+    res.json({
+        status : "ok",
+        data : student
+    })
   });
 
-app.post('/students', (req, res) => {
-    const newStudent= req.body;
-    students.push(newStudent)
+app.post('/students', async (req, res) => {
+    await Student.create(req.body) ;
+
     res.json({
         status: "ok",
         message: "new student added",
-        data: newStudent,
     })
   });
 
 
 //app listener
-app.listen(3000, () => {
-  console.log('Server started on port: ' + 3000);
+app.listen(process.env.PORT, () => {
+  console.log('Server started ');
 });
